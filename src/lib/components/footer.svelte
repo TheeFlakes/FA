6ba38dec67a72bf79265b6681a5c8e69a525e6bc
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2025-01-02 15:48:49 +0300

    updates

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -11,20 +11,20 @@
     </nav>
 
     <div class="flex justify-center space-x-5">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" />
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-new.png" alt="Facebook" />
         </a>
-        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" />
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+            <img src="https://img.icons8.com/fluent/30/000000/linkedin-2.png" alt="LinkedIn" />
         </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" />
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+            <img src="https://img.icons8.com/fluent/30/000000/instagram-new.png" alt="Instagram" />
         </a>
-        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" />
+        <a href="https://messenger.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook Messenger">
+            <img src="https://img.icons8.com/fluent/30/000000/facebook-messenger--v2.png" alt="Facebook Messenger" />
         </a>
-        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" />
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
+            <img src="https://img.icons8.com/fluent/30/000000/twitter.png" alt="Twitter" />
         </a>
     </div>
     <p class="text-center text-gray-700 font-medium">&copy; 2025 Company Ltd. All rights reserved.</p>

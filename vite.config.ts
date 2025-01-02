9138f495c188b3924aff112480185ba81cc80c07
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2025-01-02 15:57:30 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ export default {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
   },
+  root: './', // Ensure this points to the directory with index.html
+  build: {
+    outDir: 'dist', // Output directory
+  },
 };

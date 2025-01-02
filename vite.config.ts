649134bf07f8056854a9adb72f8f5c066145b87d
Flakes254 <muriuki.denis21@students.dkut.ac.ke>
2025-01-02 17:29:51 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-// vite.config.js
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
   root: './', // Ensure this points to the directory with index.html
+  server: {
+    port: 3000, // Specify the port number
+    host: '0.0.0.0', // Allow access from network
+    open: true, // Automatically open the app in the browser
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   build: {
     outDir: 'dist', // Output directory
+    rollupOptions: {
+      input: 'app.html', // specify your entry file here
+    },
   },
-};
+  // Add any other Vite configurations you need here
+});
